a367138b6c1c346d8fc4f526a9906d4d938d5b93
Ahmet Kuşdil <AhmetYusaKusdil@hotmail.com>
2021-04-14 19:08:47 +0300

    Category , About , Contact sınıfları oluşturuldu.

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+   public class About
+    {
+        public int AboutId { get; set; }
+        public string AboutDetails1 { get; set; }
+        public string AboutDetails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
+        
+
+    }
+}
---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string UserName { get; set; }
+        public string UserMail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
