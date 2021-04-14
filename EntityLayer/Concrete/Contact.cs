05b1b287ea0153a03e60e1201d1413a13de232e7
Ahmet Kuşdil <AhmetYusaKusdil@hotmail.com>
2021-04-14 19:45:08 +0300

    ilişkilerin  Oluştrurulması 2 Key Ve String Lenght Attribute

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityLayer.Concrete
 {
     public class Contact
     {
+        [Key]
         public int ContactId { get; set; }
+        [StringLength(50)]
         public string UserName { get; set; }
+        [StringLength(50)]
         public string UserMail { get; set; }
+        [StringLength(50)]
         public string Subject { get; set; }
+        [StringLength(500)]
         public string Message { get; set; }
     }
 }
--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityLayer.Concrete
 {
    public class Heading
     {
-        public int HeadingId { get; set; }
+        [Key]
+        public int HeadingId { get; set; }4
+        [StringLength(50)]
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
 
 
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
+
+
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
 
         public ICollection<Content> Contents { get; set; }
 
