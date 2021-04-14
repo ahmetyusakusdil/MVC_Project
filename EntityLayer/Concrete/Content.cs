13d6b4b79b9d54363e4c48f447266863164ebb1f
Ahmet Kuşdil <AhmetYusaKusdil@hotmail.com>
2021-04-14 18:57:47 +0300

    Entity Katmanı Sınıflarının Oluşturulması

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+   public class Content
+    {
+        public int ContentId { get; set; }
+        public string ContentText { get; set; }
+        public DateTime ContentDate { get; set; }
+     
+    }
+}
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+   public class Writer
+    {
+        public int WriterId { get; set; }
+        public string WriterName { get; set; }
+        public string WriterSurname { get; set; }
+        public string WriterImage { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set; }
+    }
+}
