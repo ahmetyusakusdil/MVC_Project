4d1f76f8a5d8a2d28cb0cb427ac0729dfeae451a
Ahmet Kuşdil <AhmetYusaKusdil@hotmail.com>
2021-04-21 18:44:27 +0300

    IRepository Metotları yazıldı.

---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        public void Add(Category c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
